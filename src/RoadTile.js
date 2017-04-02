e9ff4330b6226203ea4f4a550fdc9511daff7c14
Matt Colman <matt@mattcolman.com>
2017-04-02 11:20:45 +1000

    initial commit

---
+++ src/RoadTile.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+const defaultProps = {
+  north: false,
+  east: false,
+  south: false,
+  west: false,
+};
+
+function RoadTile({
+  north,
+  east,
+  south,
+  west,
+}) {
+  const stroke = "red";
+  const strokeWidth = 10;
+  const halfStrokeWidth = strokeWidth / 2;
+  const w = 200; // width
+  const hw = w / 2; // halfWidth
+  const hwp = hw + halfStrokeWidth; // halfWidthPlus
+  const hwm = hw - halfStrokeWidth; // halfWidthMinus
+  return (
+    <svg height={w} width={w}>
+      <rect x="0" height={w} y="0" width={w} style={{fill: "grey"}}/>
+      {north && (
+        <path d={`M${hw} 0 L${hw} ${hwp}`} stroke={stroke} strokeWidth={strokeWidth} />
+      )}
+      {east && (
+        <path d={`M0 ${hw} L${hwp} ${hw}`} stroke={stroke} strokeWidth={strokeWidth} />
+      )}
+      {south && (
+        <path d={`M${hw} ${hwm} L${hw} ${w}`} stroke={stroke} strokeWidth={strokeWidth} />
+      )}
+      {west && (
+        <path d={`M${hwm} ${hw} L${w} ${hw}`} stroke={stroke} strokeWidth={strokeWidth} />
+      )}
+    </svg>
+  );
+}
+
+RoadTile.defaultProps = defaultProps;
+export default RoadTile;
